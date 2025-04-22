0a7353efbbe12fdb387b98cbe5129bac04b874ff
krishnahw <krishnavishwakarma7823@gmail.com>
2025-04-22 16:09:11 +0530

    update database.js

--- config/dataBase.js
+++ config/dataBase.js
@@ -4,7 +4,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL,{
     dialect: 'mysql',
     dialectOptions :{
         ssl: {
-            rejctUnauthorized : false,
+            rejectUnauthorized: false
         },
     },
 });
