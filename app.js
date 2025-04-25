40ddbaf6b99aa98ea5edeb6f03437c2876a4d188
krishnahw <krishnavishwakarma7823@gmail.com>
2025-04-25 10:32:31 +0530

    set cores link in app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express();
 
 //middleware
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
