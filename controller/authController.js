ff7224199dcf8fd04fd54478649f06e559dcfcd3
krishnahw <krishnavishwakarma7823@gmail.com>
2025-04-25 19:30:04 +0530

    change in login and add the error for wrong password in authcontroller.js

--- controller/authController.js
+++ controller/authController.js
@@ -1,50 +1,66 @@
-import db from '../config/db.js';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import db from "../config/db.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 //register
 
-export const registerUser = async (req,res)=>{
-    const {username, email, password} = req.body;
+export const registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    const sql = `INSERT INTO users (username, email, password) VALUES(?, ?, ?)`;
-    db.query(sql, [username, email, hashedPassword] , (err,result) =>{
-        if(err) return res.status(500).json({message: 'Error registering user', error: err });
-        res.status(201).json({message: 'User register sucessfully', userId: result});
-    });
-
-
+  const sql = `INSERT INTO users (username, email, password) VALUES(?, ?, ?)`;
+  db.query(sql, [username, email, hashedPassword], (err, result) => {
+    if (err)
+      return res
+        .status(500)
+        .json({ message: "Error registering user", error: err });
+    res
+      .status(201)
+      .json({ message: "User register sucessfully", userId: result });
+  });
 };
-
 
 //login
 
-export const login = async (req,res) =>{
-    const {email, password} = req.body;
+export const login = async (req, res) => {
+  const { email, password } = req.body;
 
-    const sql = `SELECT * FROM users WHERE email = ?`;
+  const sql = `SELECT * FROM users WHERE email = ?`;
 
-    db.query(sql,[email], async (err,results) =>{
-        if (err) return res.status(500).json({ message: 'Error', error: err });
-        if (results.length === 0) return res.status(401).json({ message: 'Invalid credentials' });  
+  db.query(sql, [email], async (err, results) => {
+    if (err)
+      return res
+        .status(500)
+        .json({
+          message: "Error occurred while accessing the database",
+          error: err,
+        });
 
+    if (results.length === 0)
+      return res.status(401).json({ message: "Invalid credentials" });
 
-        const user = results[0];
-        console.log(user);
-        
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
+    const user = results[0];
 
+    try {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch)
+        return res.status(401).json({ message: "Invalid credentials" });
 
-        const token = jwt.sign({id: user.id},process.env.JWT_SECRET,{expiresIn: '1h'});
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
 
-        res.status(200).json({
-            message: 'Login successful',
-            token,
-            user: { id: user.id, username: user.username, email: user.email }
-          });
-
-    });
+      res.status(200).json({
+        message: "Login successful",
+        token,
+        user: { id: user.id, username: user.username, email: user.email },
+      });
+    } catch (error) {
+      console.error("Error comparing password:", error);
+      return res
+        .status(500)
+        .json({ message: "Error comparing password", error });
+    }
+  });
 };
